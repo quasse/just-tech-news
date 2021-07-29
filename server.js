fb8206ac2912ea8babdb0c89c2fe2066644c451b
Sam Morgen <samorgen14@gmail.com>
2021-07-28 20:11:18 -0700

    login and logout

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+
+require("dotenv").config();
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -15,6 +29,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
