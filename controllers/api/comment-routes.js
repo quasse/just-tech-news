8b3333d4d2c19b6d246eba2b72de598438c139c1
Sam Morgen <samorgen14@gmail.com>
2021-07-31 14:12:32 -0700

    finishing touches

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const withAuth = require("../../utils/auth");
+
 const { Comment } = require("../../models");
 
 router.get("/", (req, res) => {
@@ -10,7 +12,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
   if (req.session) {
     Comment.create({
@@ -26,7 +28,7 @@ router.post("/", (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   if (req.session) {
     Comment.destroy({
       where: {
